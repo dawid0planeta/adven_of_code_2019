1d15607b3a5eb523da30b8d2919b1757ee66b544
Dawid <dawid0planeta@gmail.com>
2020-01-05 23:47:17 +0100

    Add solution to day2

---
+++ day2/day2a.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def read_data(filename: str) -> list:
+    with open(filename, 'r') as f:
+        data = list(np.array(f.read().split(",")).astype('int32'))
+    return data
+
+
+def decode_and_run_opcode(opcode: int, a1: int, a2: int) -> int:
+    if opcode == 1:
+        return a1 + a2
+    elif opcode == 2:
+        return a1 * a2
+    else:
+        return -1       # works as a stop because input is positive
+
+def solve(filename: str) -> int:
+    data = read_data('input.txt')
+    i = 0
+    while (i < len(data)):
+        run = decode_and_run_opcode(data[i], data[data[i+1]], data[data[i+2]])
+        if run == -1:
+            break
+        data[data[i+3]] = run 
+        i += 4
+    return data[0]
+
+
+print(solve('input.txt'))
+
